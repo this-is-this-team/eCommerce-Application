bd5caa4c0f122a39092373d2fdd172df4e5f2d3a
Andrew <andrew.suponev@gmail.com>
2023-08-02 22:27:03 +0400

    refactor: fix style code with lint:fix and prettier:fix

--- src/components/test-component.js
+++ src/components/test-component.js
@@ -1,20 +1,20 @@
 /* Test modules and async */
 // TODO: Delete this file when start work
 
-import imgSVG from "../assets/rsschool-test.svg";
-import imgJPG from "../assets/rsschool-test.jpg";
+import imgSVG from '../assets/rsschool-test.svg';
+import imgJPG from '../assets/rsschool-test.jpg';
 
 export function componentImgSVG() {
-  const img = document.createElement("img");
+  const img = document.createElement('img');
   img.src = imgSVG;
-  img.width = "200"
+  img.width = '200';
   return img;
 }
 
 export function componentImgPNG() {
   const img = new Image();
   img.src = imgJPG;
-  img.width = "200"
+  img.width = '200';
   return img;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-import "./styles/index.scss";
+import './styles/index.scss';
 
-import {
-  componentImgSVG,
-  componentImgPNG,
-  sum,
-} from "./components/test-component";
+import { componentImgSVG, componentImgPNG, sum } from './components/test-component';
 
 function componentText(text) {
-  const element = document.createElement("h1");
+  const element = document.createElement('h1');
   element.textContent = text;
   return element;
 }
 
 function componentDivImg() {
-  const element = document.createElement("div");
-  element.classList.add("image");
+  const element = document.createElement('div');
+  element.classList.add('image');
   return element;
 }
 
 console.log(sum(1, 2));
 
-document.body.prepend(
-  componentText("Hello World"),
-  componentDivImg(),
-  componentImgSVG(),
-  componentImgPNG()
-);
+document.body.prepend(componentText('Hello World'), componentDivImg(), componentImgSVG(), componentImgPNG());
