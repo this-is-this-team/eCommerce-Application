269d3b049616123d8daa4e58a77ce2e2337ecac9
Andrew <andrew.suponev@gmail.com>
2023-08-11 07:02:55 +0400

    feat: implement class Banner

---
+++ src/components/banner/banner.ts
@@ -0,0 +1,19 @@
+import BaseComponent from '../../components/base-component';
+import Button from '../button/button';
+import './banner.scss';
+
+export default class Banner extends BaseComponent {
+  constructor(title: string, text: string) {
+    super('section', ['banner']);
+
+    const container = new BaseComponent('div', ['banner__container']).getElement();
+    const bannerContent = new BaseComponent('div', ['banner__content']).getElement();
+    const bannerTitle = new BaseComponent('h1', ['banner__title'], title).getElement();
+    const bannerText = new BaseComponent('p', ['banner__text'], text).getElement();
+    const bannerButton = new Button('button', 'Shop All', ['button--white'], true).getElement();
+    bannerContent.append(bannerTitle, bannerText, bannerButton);
+    container.append(bannerContent);
+
+    this.node.append(container);
+  }
+}
