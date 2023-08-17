b8f0f028a2a3739e9ad1268cf28a57bbc9720fe8
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-08-18 00:40:52 +0300

    test: add tests for a Link component

---
+++ src/components/link/link.test.ts
@@ -0,0 +1,27 @@
+import Link from './link';
+
+describe('Link', () => {
+  const link: Link = new Link('Hello', ['link-test-class'], '/login');
+
+  it('should render correct html-element', () => {
+    expect(link.getElement().outerHTML).toBe('<a class="link link-test-class" href="/login">Hello</a>');
+  });
+  it('should render correct textContent', () => {
+    expect(link.getElement().textContent).toBe('Hello');
+  });
+  it('should render correct classList', () => {
+    expect(link.getElement().classList.contains('link-test-class')).toBe(true);
+  });
+  it('should render correct href', () => {
+    expect(link.getElement().href).toBe('http://localhost/login');
+  });
+  it('should change url', () => {
+    link.getElement().click();
+    expect(window.location.pathname).toBe('/login');
+  });
+  it('should trigger a new custom event', () => {
+    const dispatchEventSpy: jest.SpyInstance = jest.spyOn(window, 'dispatchEvent');
+    link.getElement().click();
+    expect(dispatchEventSpy.mock.calls[0][0].bubbles).toEqual(true);
+  });
+});
