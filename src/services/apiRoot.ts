43f818383372e513fc3e222ac88a5fc324fbc1fe
Andrew <andrew.suponev@gmail.com>
2023-08-19 00:03:19 +0400

    chore: change node-fetch to fetch

--- src/services/apiRoot.ts
+++ src/services/apiRoot.ts
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 import { ClientBuilder, type AuthMiddlewareOptions, type HttpMiddlewareOptions } from '@commercetools/sdk-client-v2';
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 
