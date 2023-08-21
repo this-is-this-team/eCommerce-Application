9beb5383b8893819310edaad546c2c5427c5922d
Andrew <andrew.suponev@gmail.com>
2023-08-22 01:08:18 +0400

    fix: scrollTo(0,0) when switching pages

--- src/utils/change-url-event.ts
+++ src/utils/change-url-event.ts
@@ -4,4 +4,5 @@ export function changeUrlEvent(href: AppRoutesPath): void {
   window.history.pushState({}, '', href);
   const changeURL = new CustomEvent('changeURL', { bubbles: true });
   window.dispatchEvent(changeURL);
+  window.scrollTo(0, 0);
 }
