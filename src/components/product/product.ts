dd3270c5489123bf8caa820c603ab90dd85b55bd
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-08-31 23:08:37 +0300

    feat: draw routes and rest-info about product

--- src/components/product/product.ts
+++ src/components/product/product.ts
@@ -1,8 +1,8 @@
 import getProductById from '../../services/getProductById';
 import BaseComponent from '../base-component';
 import Button from '../button/button';
-import './product.scss';
 import { ProductData } from '@commercetools/platform-sdk';
+import './product.scss';
 
 interface ProductAttributes {
   [name: string]: string;
@@ -25,7 +25,7 @@ export default class Product extends BaseComponent<'div'> {
   constructor(id: string) {
     super('div', ['product']);
 
-    this.addToCartBtn = new Button('submit', 'Add To Card').getElement();
+    this.addToCartBtn = new Button('button', 'Add To Card', [], false, () => this.addToCart(id)).getElement();
 
     this.createMarkup(id);
   }
@@ -34,32 +34,28 @@ export default class Product extends BaseComponent<'div'> {
     await this.getProductData(id);
     this.getProductAttributes();
 
-    if (this.productData) {
-      const productInfo = this.drawProductInfo();
-      const productForm = this.drawProductForm();
-      const productAbout = this.drawProductAbout();
+    const productInfo = this.drawProductInfo();
+    const productForm = this.drawProductForm();
+    const productAbout = this.drawProductAbout();
 
-      this.node.append(productInfo, productForm, productAbout);
-    }
+    this.node.append(productInfo, productForm, productAbout);
   }
 
   private drawProductInfo() {
-    console.log(this.productData);
     const title = this.productData?.name.en || 'This is the best tour in your life!';
+    const { rating, reviews, inStock, shortDescription, adventureStyle } = this.productAttributes;
 
-    const { rating, reviews, inStock, shortDescription } = this.productAttributes;
-
-    const productInfo = new BaseComponent('div', ['product__main-info']).getElement();
+    const productRoutes = new BaseComponent('p', ['product__routes'], adventureStyle).getElement();
+    const productInfo = new BaseComponent('div', ['product__info']).getElement();
     const productOpinion = new BaseComponent('div', ['product__opinion']).getElement();
     const productTitle = new BaseComponent('h2', ['product__title'], title).getElement();
     const productRating = new BaseComponent('p', ['product__rating'], rating).getElement();
-    const productReviews = new BaseComponent('p', ['product__reviews'], `${reviews} Reviews`).getElement();
-    const productDescription = new BaseComponent('p', ['product__subtitle'], shortDescription).getElement();
+    const productReviews = new BaseComponent('p', ['product__reviews'], `${reviews}+ Reviews`).getElement();
+    const productDescription = new BaseComponent('p', ['product__description'], shortDescription).getElement();
     const productStock = new BaseComponent('p', ['product__stock'], `${inStock} In Stock`).getElement();
 
     productOpinion.append(productRating, productReviews);
-
-    productInfo.append(productTitle, productOpinion, productDescription, productStock);
+    productInfo.append(productRoutes, productTitle, productOpinion, productDescription, productStock);
 
     return productInfo;
   }
@@ -77,7 +73,7 @@ export default class Product extends BaseComponent<'div'> {
   }
 
   private drawProductForm() {
-    const productForm = new BaseComponent('form', ['product__form']).getElement();
+    const productForm = new BaseComponent('div', ['product__add-to-cart']).getElement();
 
     productForm.append(this.addToCartBtn);
 
@@ -85,9 +81,31 @@ export default class Product extends BaseComponent<'div'> {
   }
 
   private drawProductAbout() {
+    const { aboutTour, location } = this.productAttributes;
+
     const productAbout = new BaseComponent('ul', ['product__about']).getElement();
+    const productAboutItem = new BaseComponent('li', ['product__about-item'], 'About Tour').getElement();
+    const productLocationItem = new BaseComponent('li', ['product__about-item'], 'Location').getElement();
+
+    aboutTour
+      .slice(0, -1)
+      .split('.')
+      .forEach((sentence) => {
+        const aboutTourSentence = new BaseComponent('p', ['product__description'], `${sentence}.`).getElement();
+
+        productAboutItem.append(aboutTourSentence);
+      });
+
+    const productLocation = new BaseComponent('p', ['product__description'], location).getElement();
+
+    productLocationItem.append(productLocation);
+    productAbout.append(productAboutItem, productLocationItem);
 
     return productAbout;
+  }
+
+  private addToCart(id: string) {
+    console.log(`TODO: create func add to cart. Product ID: ${id}`);
   }
 
   private async getProductData(id: string) {
