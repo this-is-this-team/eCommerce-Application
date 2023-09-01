459754bc8bf2ae9dac6ebc87360578ea933abc63
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-09-01 16:30:04 +0300

    feat: add List component

---
+++ src/components/list/list.ts
@@ -0,0 +1,26 @@
+import BaseComponent from '../base-component';
+import { ListDataType } from '../../types/types';
+import './list.scss';
+
+export default class List extends BaseComponent<'ul'> {
+  constructor(data: ListDataType) {
+    super('ul', ['list']);
+    this.node.append(...this.createList(data));
+  }
+
+  private createList(data: ListDataType): HTMLElement[] {
+    const listOfElements: HTMLElement[] = [];
+
+    for (const key in data) {
+      const newElement = new BaseComponent('li', ['list-item']).getElement();
+      const newElementKey = new BaseComponent('li', ['list-item__key'], key).getElement();
+      const newElementValue = new BaseComponent('li', ['list-item__value'], data[key]).getElement();
+
+      newElement.append(newElementKey, newElementValue);
+
+      listOfElements.push(newElement);
+    }
+
+    return listOfElements;
+  }
+}
