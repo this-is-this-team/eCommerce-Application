565f1fd717441c1cc110155c35969e505deb1c53
Andrew <andrew.suponev@gmail.com>
2023-09-03 11:24:52 +0400

    chore: add different photos for different category pages

--- src/components/hero-shop/hero-shop.ts
+++ src/components/hero-shop/hero-shop.ts
@@ -20,6 +20,22 @@ export default class HeroShop extends BaseComponent<'section'> {
     ).getElement();
     const imageBlock = new BaseComponent('div', ['shop-hero__image']).getElement();
 
+    if (titleText === 'Europe') {
+      imageBlock.classList.add('shop-hero__image--europe');
+    } else if (titleText === 'Asia') {
+      imageBlock.classList.add('shop-hero__image--asia');
+    } else if (titleText === 'Africa') {
+      imageBlock.classList.add('shop-hero__image--africa');
+    } else if (titleText === 'America') {
+      imageBlock.classList.add('shop-hero__image--america');
+    } else if (titleText === 'Hiking & Trekking') {
+      imageBlock.classList.add('shop-hero__image--hiking');
+    } else if (titleText === 'Sailing') {
+      imageBlock.classList.add('shop-hero__image--sailing');
+    } else if (titleText === 'Safari') {
+      imageBlock.classList.add('shop-hero__image--africa');
+    }
+
     content.append(title, text);
     wrapper.append(content, imageBlock);
     container.append(wrapper);
