ed2c927e103ae3e721a515247eb80927215622f1
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-09-05 22:37:02 +0300

    feat: implement modal slider and refactor: make code more readable

--- src/components/slider/slider.ts
+++ src/components/slider/slider.ts
@@ -8,22 +8,45 @@ import './slider.scss';
 
 export default class ProductSlider extends BaseComponent<'div'> {
   private images: Image[];
+  private sliderView: HTMLElement;
+  private sliderList: HTMLElement;
+  private sliderBtnNext: HTMLElement;
+  private sliderBtnPrev: HTMLElement;
+  private modalWindow: HTMLElement;
 
   constructor(images: Image[]) {
     super('div', ['product__slider']);
     this.images = images;
 
+    const mainComponents = this.initMainComponents();
+    this.sliderView = mainComponents.sliderView;
+    this.sliderList = mainComponents.sliderList;
+    this.sliderBtnNext = mainComponents.sliderBtnNext;
+    this.sliderBtnPrev = mainComponents.sliderBtnPrev;
+    this.modalWindow = new BaseComponent('div', ['popup', 'open']).getElement();
+    this.modalWindow.addEventListener('click', this.onBackdropClick.bind(this));
+
     this.createMarkup();
   }
 
-  private createMarkup(): void {
+  private initMainComponents() {
     const sliderView = new BaseComponent('div', ['swiper', 'swiper-view']).getElement();
-    const swiperViewWrapper = new BaseComponent('div', ['swiper-wrapper']).getElement();
-
     const sliderBtnNext = new BaseComponent('div', ['swiper-button-next']).getElement();
     const sliderBtnPrev = new BaseComponent('div', ['swiper-button-prev']).getElement();
-
     const sliderList = new BaseComponent('div', ['swiper', 'swiper-list']).getElement();
+
+    return {
+      sliderView,
+      sliderBtnNext,
+      sliderBtnPrev,
+      sliderList,
+    };
+  }
+
+  private createMarkup(): void {
+    const swiperViewWrapper = new BaseComponent('div', ['swiper-wrapper']).getElement();
+    this.setOpenListener(swiperViewWrapper);
+
     const swiperListWrapper = new BaseComponent('div', ['swiper-wrapper']).getElement();
 
     for (let i = 0; i < this.images.length; i++) {
@@ -42,12 +65,16 @@ export default class ProductSlider extends BaseComponent<'div'> {
       swiperListWrapper.append(imageListContainer);
     }
 
-    sliderView.append(swiperViewWrapper, sliderBtnNext, sliderBtnPrev);
-    sliderList.append(swiperListWrapper);
+    this.sliderView.append(swiperViewWrapper, this.sliderBtnNext, this.sliderBtnPrev);
+    this.sliderList.append(swiperListWrapper);
 
-    this.node.append(sliderView, sliderList);
+    this.node.append(this.sliderView, this.sliderList);
 
-    const swiperList = new Swiper(sliderList, {
+    this.initSwiper();
+  }
+
+  private initSwiper() {
+    const swiperList = new Swiper(this.sliderList, {
       loop: true,
       spaceBetween: 10,
       slidesPerView: 4,
@@ -55,17 +82,64 @@ export default class ProductSlider extends BaseComponent<'div'> {
       watchSlidesProgress: true,
     });
 
-    new Swiper(sliderView, {
+    new Swiper(this.sliderView, {
       loop: true,
       spaceBetween: 24,
       modules: [Navigation, Thumbs],
       navigation: {
-        nextEl: sliderBtnNext,
-        prevEl: sliderBtnPrev,
+        nextEl: this.sliderBtnNext,
+        prevEl: this.sliderBtnPrev,
       },
       thumbs: {
         swiper: swiperList,
       },
     });
+  }
+
+  private setOpenListener(element: HTMLElement) {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      if (event.target instanceof HTMLElement) {
+        const firstSlide: HTMLElement | null = event.target.closest('.swiper-slide-active');
+        const imageIndex = Number(firstSlide?.getAttribute('data-swiper-slide-index') || 0);
+
+        this.openModal(imageIndex);
+      }
+    });
+  }
+
+  private openModal(index: number): void {
+    const modalSliderContainer = new BaseComponent('div', ['product__slider_modal']).getElement();
+    const modalSliderView = <HTMLElement>this.sliderView.cloneNode(true);
+
+    modalSliderContainer.append(modalSliderView);
+    this.modalWindow.append(modalSliderContainer);
+
+    document.body.append(this.modalWindow);
+
+    new Swiper(modalSliderView, {
+      loop: true,
+      spaceBetween: 24,
+      modules: [Navigation],
+      navigation: {
+        nextEl: '.popup .swiper-button-next',
+        prevEl: '.popup .swiper-button-prev',
+      },
+      initialSlide: index,
+    });
+  }
+
+  onBackdropClick(event: MouseEvent): void {
+    event.preventDefault();
+
+    if (event.target instanceof HTMLElement) {
+      if (event.target.classList.contains('popup')) this.removeModalWindow();
+    }
+  }
+
+  private removeModalWindow(): void {
+    this.modalWindow.innerHTML = '';
+    this.modalWindow.remove();
   }
 }
