152cec7815aec5dc2205329d870c342715475fb6
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-09-09 14:25:05 +0300

    feat: create basket page with empty cart message

--- src/pages/basket-page/basket-page.ts
+++ src/pages/basket-page/basket-page.ts
@@ -3,6 +3,8 @@ import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
 import { IBreadcrumbLink } from '../../types/interfaces';
 import { AppRoutesPath } from '../../router/types';
 import './basket-page.scss';
+import Button from '../../components/button/button';
+import { changeUrlEvent } from '../../utils/change-url-event';
 
 const breadcrumbsLinks: IBreadcrumbLink[] = [
   {
@@ -19,18 +21,40 @@ export default class BasketPage extends BaseComponent<'div'> {
     super('div', ['basket-page']);
 
     this.breadcrumbs = new Breadcrumbs(breadcrumbsLinks, 'Basket').getElement();
-
-    this.basketSection = new BaseComponent('section', ['basket-section']).getElement();
-    this.renderBasketSection();
+    this.basketSection = this.renderBasketSection();
 
     this.node.append(this.breadcrumbs, this.basketSection);
   }
 
-  private renderBasketSection(): void {
-    const container: HTMLElement = new BaseComponent('div', ['basket-section__container']).getElement();
+  private renderBasketSection(): HTMLElement {
+    const basketSection = new BaseComponent('section', ['basket-section']).getElement();
+    const basketSectionContainer = new BaseComponent('div', ['basket-section__container']).getElement();
+    const basketSectionTitle = new BaseComponent(
+      'h3',
+      ['basket-section__title'],
+      'Your Shopping Cart is Empty'
+    ).getElement();
 
-    // TODO: implement the cart components and add to the container in the following basket-issues
+    const basketContent = new BaseComponent('div', ['basket-section__content']).getElement();
+    const basketIcon = new BaseComponent('div', ['basket-section__icon']).getElement();
+    const contentTitle = new BaseComponent(
+      'h4',
+      ['basket-section__content-title'],
+      'Your Shopping Cart is Empty'
+    ).getElement();
+    const basketSubtitle = new BaseComponent(
+      'p',
+      ['basket-section__subtitle'],
+      'Looks like you haven’t added anything yet, let’s get you started!'
+    ).getElement();
+    const basketBtn = new Button('button', 'Start Shopping', ['basket-section__button'], false, () =>
+      changeUrlEvent(AppRoutesPath.SHOP)
+    ).getElement();
 
-    this.basketSection.append(container);
+    basketContent.append(basketIcon, contentTitle, basketSubtitle, basketBtn);
+    basketSectionContainer.append(basketSectionTitle, basketContent);
+    basketSection.append(basketSectionContainer);
+
+    return basketSection;
   }
 }
