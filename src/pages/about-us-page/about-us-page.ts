bce4a02497a49221a613fb584d62b1e541a1f2c9
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-09-11 16:22:31 +0300

    feat: add about-us list with members

--- src/pages/about-us-page/about-us-page.ts
+++ src/pages/about-us-page/about-us-page.ts
@@ -2,6 +2,7 @@ import BaseComponent from '../../components/base-component';
 import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
 import { IBreadcrumbLink } from '../../types/interfaces';
 import { AppRoutesPath } from '../../router/types';
+import AboutUsList from '../../components/about-us/about-us-list';
 import './about-us-page.scss';
 
 const breadcrumbsLinks: IBreadcrumbLink[] = [
@@ -20,14 +21,25 @@ export default class AboutUsPage extends BaseComponent<'div'> {
 
     this.breadcrumbs = new Breadcrumbs(breadcrumbsLinks, 'About Us').getElement();
 
-    this.aboutUsSection = new BaseComponent('section', ['about-us-section']).getElement();
+    this.aboutUsSection = new BaseComponent('section', ['about-us-page__section']).getElement();
     this.renderAboutUsSection();
 
     this.node.append(this.breadcrumbs, this.aboutUsSection);
   }
 
   private renderAboutUsSection(): void {
-    const container: HTMLElement = new BaseComponent('div', ['about-us-section__container']).getElement();
+    const container: HTMLElement = new BaseComponent('div', ['about-us-page__container']).getElement();
+    const sectionTitle = new BaseComponent(
+      'h1',
+      ['about-us-page__title'],
+      'Discover Our Team: Meet the Faces'
+    ).getElement();
+    const sectionSubtitle = new BaseComponent('h2', ['about-us-page__subtitle'], 'Behind ').getElement();
+    sectionSubtitle.insertAdjacentHTML('beforeend', '<span>this is this.</span>');
+
+    const aboutUsList = new AboutUsList().getElement();
+
+    container.append(sectionTitle, sectionSubtitle, aboutUsList);
 
     // TODO: implement the about-us components and add to the container in the following about-us-issues
 
