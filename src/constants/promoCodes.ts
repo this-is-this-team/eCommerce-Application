a32614cdea9ec2a44ad28a180ba34e5300ac16d3
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-09-14 23:55:46 +0300

    refactor: change expirationDate in  promoCodes

--- src/constants/promoCodes.ts
+++ src/constants/promoCodes.ts
@@ -5,25 +5,25 @@ const promoCodes: IPromoCodes[] = [
     title: 'Family Getaway Deal: Kids Stay Free',
     descr: 'Have an unforgettable family getaway where kids stay for free with promo code FAMILYFUN.',
     code: 'FAMILYFUN',
-    expirationDate: '09/15/2023, 11:59 PM',
+    expirationDate: '09/25/2023, 11:59 PM',
   },
   {
     title: 'Last-Minute Wanderlust: 10% Off Any Booking',
     descr: 'Embrace your last-minute wanderlust and enjoy 10% off any booking with promo code LASTCHANCE.',
     code: 'LASTCHANCE',
-    expirationDate: '09/15/2023, 11:59 PM',
+    expirationDate: '10/01/2023, 11:59 PM',
   },
   {
     title: 'Cruise in Style: $100 Off Ocean View Cabins',
     descr: 'Cruise in style! Get $100 off ocean view cabins with promo code CRUISE100.',
     code: 'CRUISE100',
-    expirationDate: '09/15/2023, 11:59 PM',
+    expirationDate: '10/08/2023, 11:59 PM',
   },
   {
     title: 'Group Adventure: 25% Off for Parties of 4 or More',
     descr: 'Plan a group adventure and enjoy a 25% discount for parties of 4 or more with promo code GROUP24.',
     code: 'GROUP24',
-    expirationDate: '09/15/2023, 11:59 PM',
+    expirationDate: '12/25/2023, 11:59 PM',
   },
 ];
 
