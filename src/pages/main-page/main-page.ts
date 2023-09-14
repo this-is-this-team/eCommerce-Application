a04b59e811d31f2544c69ee0e64c446b60fdc097
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-09-15 00:13:21 +0300

    refactor: remove temporarySection in main page

--- src/pages/main-page/main-page.ts
+++ src/pages/main-page/main-page.ts
@@ -11,16 +11,14 @@ const bannerContent: IBannerContent = {
 
 export default class MainPage extends BaseComponent<'div'> {
   private bannerSection: HTMLElement;
-  private temporarySection: HTMLElement;
   private promoCodesSection: HTMLElement;
 
   constructor() {
     super('div', ['main-page']);
 
     this.bannerSection = new Banner(bannerContent.title, bannerContent.text, ['banner--main-page']).getElement();
-    this.temporarySection = new BaseComponent('section', ['temporary-section'], 'Opening soon').getElement();
     this.promoCodesSection = new PromoCodes().getElement();
 
-    this.node.append(this.bannerSection, this.temporarySection, this.promoCodesSection);
+    this.node.append(this.bannerSection, this.promoCodesSection);
   }
 }
