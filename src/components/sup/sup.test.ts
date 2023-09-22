b5264123d15c47002f6719e552bfab3bd0f888f7
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-09-22 10:54:45 +0300

    refactor: remove getElement in all test-cases

--- src/components/sup/sup.test.ts
+++ src/components/sup/sup.test.ts
@@ -1,15 +1,15 @@
 import Sup from './sup';
 
 describe('Sup', () => {
-  const sup: Sup = new Sup('Sub text', ['sup-test-class']);
+  const sup: HTMLElement = new Sup('Sub text', ['sup-test-class']).getElement();
 
   it('should render correct html-element', () => {
-    expect(sup.getElement().outerHTML).toBe('<sup class="sup sup-test-class">Sub text</sup>');
+    expect(sup.outerHTML).toBe('<sup class="sup sup-test-class">Sub text</sup>');
   });
   it('should render correct textContent', () => {
-    expect(sup.getElement().textContent).toBe('Sub text');
+    expect(sup.textContent).toBe('Sub text');
   });
   it('should render correct classList', () => {
-    expect(sup.getElement().classList.contains('sup-test-class')).toBe(true);
+    expect(sup.classList.contains('sup-test-class')).toBe(true);
   });
 });
